356bc97174d2308adbe1e6c7414eb3206bbed178
Wilson-Rodriguez <wilsonalex2002@gmail.com>
2025-08-12 17:45:22 -0600

    Ajuste de texto

--- schemas/comentarios.schema.js
+++ schemas/comentarios.schema.js
@@ -4,7 +4,7 @@ import { sanitizarEntrada } from '../utils/sanitizar.js'
 const comentarioSchema = zod.object({
     "contenido": zod.string({error: "El contenido es obligatorio"})
     .min(1, "El comentario no puede estar vacio")
-    .max(300, "El comentario no puede exceder los 150 caracteres")
+    .max(300, "El comentario no puede exceder los 300 caracteres")
     .transform(sanitizarEntrada)
 }).strict()
 
